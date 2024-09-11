979a75420f2e63f1b411075124734a34fe0c81e5
Ansen Kao <trim2589@gmail.com>
2024-09-12 00:17:19 +0800

    fix: cuda settings

--- thsr_ticket/ml/run.py
+++ thsr_ticket/ml/run.py
@@ -21,27 +21,33 @@ conf_mdname=conf['conf_mdname']
 def run(img):
     img_li = list()
     label_li = list()
-    # print(img_list)
-    # img = Image.open(imgpath)
+    
     trans = ToTensor()
     img_tensor = trans(img)
+    
     cnn = CNN()
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    cnn.to(device)
+    cnn.eval()
+    
     if torch.cuda.is_available():
-        cnn = cnn.cuda()
-        cnn.eval()
         cnn.load_state_dict(torch.load(conf_mdname))
     else:
-        cnn.eval()
         model = torch.load(conf_mdname, map_location='cpu')
         cnn.load_state_dict(model)
-    img_tensor = img_tensor.view(1, 3, conf_w, conf_h)
-    output = cnn(img_tensor)
-    output = output.view(-1, conf_set)
-    output = nn.functional.softmax(output, dim=1)
-    output = torch.argmax(output, dim=1)
-    output = output.view(-1, conf_len)[0]
+    
+    img_tensor = img_tensor.view(1, 3, conf_w, conf_h).to(device)
+    
+    with torch.no_grad():
+        output = cnn(img_tensor)
+        output = output.view(-1, conf_set)
+        output = nn.functional.softmax(output, dim=1)
+        output = torch.argmax(output, dim=1)
+        output = output.view(-1, conf_len)[0]
+    
     label = ''.join([alphabet[i] for i in output.cpu().numpy()])
-    print (label)
+    print(label)
     return label
  
 if __name__=="__main__":
